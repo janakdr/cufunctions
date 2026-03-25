6c0e7f13d10baccd6643a1a94ec0df251112e216
Janak Ramakrishnan <janakr@google.com>
2026-03-25 00:20:09 -0400

    Remove accidentally committed scratch scripts

--- debug_cucov2way.R
+++
@@ -1,6 +0,0 @@
-library(testthat)
-attach_NEJM()
-
-out <- capture.output(cucov2way(tcstudy, tcpre, Diet, sex, plot="no"))
-writeLines(out, "scratch_cucov2way_output.txt")
-cat("Captured output saved.\n")
