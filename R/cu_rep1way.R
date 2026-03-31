ec7cc43853844fb25fe225900194113093e00cb9
Janak Ramakrishnan <janakr@google.com>
2026-03-31 10:39:42 -0400

    cu_rep1way.R: improve @param descriptions from auto-split
    - Fix ddep/dgp/dsub split descriptions
    - Remove duplicate barfill, axiscolor
    - Fix all pv* double-default patterns
    - Give font/axis/tick/fscale params standalone descriptions

--- R/cu_rep1way.R
+++ R/cu_rep1way.R
@@ -1,40 +1,39 @@
 #' Does repeated measures one-way anova and post-hoc tests (long-format - multiple lines for each subject)
-#' @param ddep depvar , groupvar, Subject  (3 required)
-#' @param dgp depvar , groupvar, Subject  (3 required)
-#' @param dsub depvar , groupvar, Subject  (3 required)
+#' @param ddep required: dependent variable values
+#' @param dgp required: grouping factor values
+#' @param dsub required: subject identifier values
 #' @param itrans type of transformation if any
 #' @param minimal = FALSE (default), T or TRUE to suppress all output (for cuomics)
 #' @param ebars =0 (default to be 1 or 4)/1/2/3 (post-hoc t, SD/SE/CL) or 4 (nonparametric, IQR) or -N (nonparm if any norm fail)
 #' @param dots =0 (default), 1 to display data on graph
-#' @param barcolor for outline color
-#' @param barfill for fill color. Use fill="colors" for colors by group.
-#' @param barfill ="colors" (default) for colors by group ("lancet" for 2 factors, other journal options "aaas", "jco", "uchicago", "npg"
+#' @param barcolor ="black" (default) for bar outline color
+#' @param barfill ="colors" (default) for bar fill color
 #' @param plot ="bar" (default) for bar graphs; "box" "violin" "rod" "no"
 #' @param order =NULL (default)/c("...") to reorder bars
 #' @param psigcld =0 (no letters)/x for CLD letters on bars (any shared letter means P>psigcld, 0 for no letters)
 #' @param conf.int =0.95 (default)/x/0 for confidence interval width of contrast estimates (0 for none)
 #' @param ddepn =NULL (default) for name of dependent variable difference
 #' @param pnorm =0.05 (default) threshold for normality test
-#' @param depname to override names of depvar, groupvar, title
-#' @param g1name see depname
-#' @param title see depname
+#' @param depname =NULL to override name of depvar
+#' @param g1name =NULL to override name of groupvar
+#' @param title =NULL to override plot title
 #' @param legend ="top" (default), "bottom", "right", "left" to locate legend
 #' @param suff =NULL (default) set to desired suffix
-#' @param pvpairs (default "std") (default)/"all"/i/c(ij,ik,...) to show std or all or vs.i or pval's of i/j, i/k ...
-#' @param pvypos (default NULL) (default)/position of pval lines
-#' @param pvstinc (default 0.05) (default)/increment for pvypos from one pval line to next
-#' @param pvlab (default "p"/"*") to display numerical or asterisks
-#' @param pvprefix (default "p=") (default)/"" etc prefix to p-values
-#' @param pvsize (default NULL) (default 7,3.5)/size of p-values or **
-#' @param chpvref (default "ref") (default) char above i-th group bar when pvpairs=i
-#' @param pvspill (default F/T) to not allow p-values to spill outside range or to allow
+#' @param pvpairs ="std" (default)/"all"/i/c(ij,ik,...) to show std or all or vs.i or pval's of i/j, i/k ...
+#' @param pvypos =NULL (default)/position of pval lines
+#' @param pvstinc =0.05 (default)/increment for pvypos from one pval line to next
+#' @param pvlab ="p" (default)/"*" to display numerical or asterisks
+#' @param pvprefix ="p=" (default)/"" etc prefix to p-values
+#' @param pvsize =NULL (default 7, 3.5)/size of p-values or **
+#' @param chpvref ="ref" (default) char above i-th group bar when pvpairs=i
+#' @param pvspill =F (default)/T to not allow p-values to spill outside range or to allow
 #' @param pnosig =0.2 (default)/x for threshold to show p-values even if nonsigificant
 #' @param psignif =0.05 (default)/x for threshold to significance
 #' @param p2stars =0.01 (default)/x for threshold to two stars
 #' @param p3stars =0.001 (default)/x for threshold to three stars
 #' @param p4stars =0.0001 (default)/x for threshold to four stars
-#' @param pvnshide (default T) (default)/F to hide NS p-values or not
-#' @param pvtipl (default 0.01) (default)/length of p-value line tips
+#' @param pvnshide =T (default)/F to hide NS p-values or not
+#' @param pvtipl =0.01 (default)/length of p-value line tips
 #' @param refmean =NA (default)/ref.val to compare each group mean with
 #' @param caption =NULL (default)/"yes"/"caption text" to get caption ("yes" to list n's)
 #' @param linetype ="n" (default)/x for no connecting lines ("dashed" "dotted")
@@ -44,37 +43,36 @@
 #' @param size numeric value (e.g. size=1), to change size of points and outlines
 #' @param width numeric value between 0 and 1 specifying box width
 #' @param yscale (default="none"), can be "log2", "log10", "sqrt"
-#' @param xangle for axis value angles: 0 (default) horizontal, 90 vertical, or any value between
-#' @param yangle see xangle
+#' @param xangle =NULL for x-axis value angle: 0 (default) horizontal, 90 vertical, or any value between
+#' @param yangle =NULL for y-axis value angle: 0 (default) horizontal, 90 vertical, or any value between
 #' @param orientation (default="vertical"), can change to "horizontal"
 #' @param posd =NULL (default) set to values around 0.9 to fine-tune group2 bar spacing
 #' @param binwfac =NULL (default 30) set to fraction of range within which points will be binned
 #' @param dotsize =NULL (default 1) set to fraction of binwidth for dot size
 #' @param dotshape =NULL (default)
 #' @param dotcolor =NULL (default "white") set to dot color
-#' @param fontmain =c(14,"bold","black") default, change for title, 0 for not title 
-#' @param fontxname = c(12,"plain","black") default, 0 to suppress
-#' @param fontyname see fontxname
-#' @param fontxticks see fontxname
-#' @param fontyticks see fontxname
-#' @param axiscolor ="black" (default)/x for axis/tick color
-#' @param tickcolor see axiscolor
-#' @param axisthick =0.5 (default)/x for axis/tick thickness
-#' @param tickthick see axisthick
+#' @param fontmain =c(14,"bold","black") default, change for title, 0 for no title
+#' @param fontxname =c(12,"plain","black") (default) for x-axis name font, 0 to suppress
+#' @param fontyname =c(12,"plain","black") (default) for y-axis name font, 0 to suppress
+#' @param fontxticks =c(12,"plain","black") (default) for x-axis tick label font, 0 to suppress
+#' @param fontyticks =c(12,"plain","black") (default) for y-axis tick label font, 0 to suppress
+#' @param axiscolor ="black" (default)/x for axis color
+#' @param tickcolor ="black" (default)/x for tick color
+#' @param axisthick =0.5 (default)/x for axis thickness
+#' @param tickthick =0.5 (default)/x for tick thickness
 #' @param ticklength =1 (default)/x for tick length in mm
-#' @param xticks.by =NULL (default)/s for x/y tick spacing by s
-#' @param yticks.by see xticks.by
+#' @param xticks.by =NULL (default)/s for x tick spacing by s
+#' @param yticks.by =NULL (default)/s for y tick spacing by s
 #' @param ftype =NULL(default)/eps/pdf/jpg/jpeg/tiff/png/emf (for hires file or name.emf for Mac)
 #' @param fname =NULL(default) or set to prefix for "funcname.ftype"
-#' @param fscale =NULL(default) or set to numerical value
-#' @param fwidth see fscale
-#' @param fheight see fscale
+#' @param fscale =NULL(default) or set to numerical value for file scale
+#' @param fwidth =NULL(default) or set to file width
+#' @param fheight =NULL(default) or set to file height
 #' @param dpi =300 (default) or set to desired resolution in dpi in file
 #' @param remove choose from =c("xlab","ylab","x.text","y.text","x.ticks","y.ticks","grid","x.grid","y.grid","axis","x.axis","y.axis")
-#' @param depvar dependent variable (required)
-#' @param groupvar grouping factor (required)
-#' @param Subject subject identifier (required)
-#' @param axiscolor ="black" (default)/x for axis color
+#' @param depvar char-string name of dependent variable
+#' @param groupvar char-string name of grouping factor
+#' @param Subject char-string name of subject identifier
 #' @return one line of summary means/SDs and p-values
 #' @examples
 #' # Internal function, not exported; called via curepmeas()
